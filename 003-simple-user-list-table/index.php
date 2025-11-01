993e1e5a18e05daca8be111e461358ca3c3b9ca5
SadraYavar <sadra.yavarzadeh@gmail.com>
2025-11-01 21:33:35 +0330

    pagination

--- 003-simple-user-list-table/index.php
+++ 003-simple-user-list-table/index.php
@@ -1,6 +1,7 @@
 <?php include 'main.php'; ?>
 <!DOCTYPE html>
 <html lang="fa">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,6 +10,7 @@
     <link rel="stylesheet" href="https://dl.daneshjooyar.com/mvie/Moodi_Hamed/assets/css/font-yekanbakh-vf.css">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="container">
         <div class="table-container">
@@ -37,65 +39,73 @@
 
             <table class="table table-hover table-striped table-responsive">
                 <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">نام</th>
-                    <th scope="col">نام خانوادگی</th>
-                    <th scope="col">تلفن</th>
-                    <th scope="col">
-                        <a href="#" class="order">
-                            سن
-                        </a>
-                    </th>
-                    <th scope="col">وضعیت</th>
-                </tr>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">نام</th>
+                        <th scope="col">نام خانوادگی</th>
+                        <th scope="col">تلفن</th>
+                        <th scope="col">
+                            <a href="#" class="order">
+                                سن
+                            </a>
+                        </th>
+                        <th scope="col">وضعیت</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>حامد</td>
-                    <td>مودی</td>
-                    <td>09123456789</td>
-                    <td>32</td>
-                    <td>
-                        <span class="badge text-bg-success">فعال</span>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>علی</td>
-                    <td>رمضانی</td>
-                    <td>09123456789</td>
-                    <td>32</td>
-                    <td>
-                        <span class="badge text-bg-danger">غیرفعال</span>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>حسن</td>
-                    <td>رجبیان</td>
-                    <td>09123456789</td>
-                    <td>32</td>
-                    <td>
-                        <span class="badge text-bg-danger">غیرفعال</span>
-                    </td>
-                </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>حامد</td>
+                        <td>مودی</td>
+                        <td>09123456789</td>
+                        <td>32</td>
+                        <td>
+                            <span class="badge text-bg-success">فعال</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>علی</td>
+                        <td>رمضانی</td>
+                        <td>09123456789</td>
+                        <td>32</td>
+                        <td>
+                            <span class="badge text-bg-danger">غیرفعال</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>حسن</td>
+                        <td>رجبیان</td>
+                        <td>09123456789</td>
+                        <td>32</td>
+                        <td>
+                            <span class="badge text-bg-danger">غیرفعال</span>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
 
             <nav>
                 <ul class="pagination">
-                    <li class="page-item disabled">
-                        <span class="page-link">Previous</span>
+                    <li class="page-item<?php echo $current_page === 1 ? " disabled" : "" ?>">
+                        <a class="page-link" href="?page=<?php echo $current_page - 1 ?>">Previous</a>
                     </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+
+                    <?php if ($current_page !== 1): ?>
+                        <li class="page-item"><a class="page-link" href="?page=<?php echo $current_page - 1 ?>"><?php echo $current_page - 1 ?></a></li>
+                    <?php endif; ?>
+
                     <li class="page-item active" aria-current="page">
-                        <span class="page-link">2</span>
+                        <span class="page-link"><?php echo $current_page ?></span>
                     </li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                        <a class="page-link" href="#">Next</a>
+
+                    <?php if ($current_page !== $last_page): ?>
+                        <li class="page-item"><a class="page-link" href="?page=<?php echo $current_page + 1 ?>"><?php echo $current_page + 1 ?></a></li>
+                    <?php endif; ?>
+
+                    <li class="page-item<?php echo $current_page === $last_page ? " disabled" : "" ?>">
+                        <a class="page-link" href="?page=<?php echo $current_page + 1 ?>">Next</a>
                     </li>
                 </ul>
             </nav>
@@ -104,4 +114,5 @@
 
     </div>
 </body>
+
 </html>
--- 003-simple-user-list-table/main.php
+++ 003-simple-user-list-table/main.php
@@ -1,3 +1,7 @@
 <?php
 
 $users = include 'data.php';
+
+// pagination
+$current_page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+$last_page = (int) ceil(count($users) / 2);
